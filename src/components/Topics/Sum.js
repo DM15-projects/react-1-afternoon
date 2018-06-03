81f3050e291014817bab7bac958858974b226433
JonathanWaller <waller.j27@gmail.com>
2018-06-03 12:06:24 -0500

    complete

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,8 +1,55 @@
 import React, { Component } from "react";
 
 class Sum extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: "",
+      firstInput: "",
+      secondInput: ""
+    };
+  }
+
+  firstChange(val) {
+    this.setState({ firstInput: val });
+  }
+
+  secondChange(val) {
+    this.setState({ secondInput: val });
+  }
+
+  addSum(num1, num2) {
+    let total = parseInt(num1) + parseInt(num2);
+    this.setState({ results: total });
+  }
+
   render() {
-    return <p>Sum</p>;
+    return (
+      <div className="puzzleBox sumPB">
+        <h4>Sum</h4>
+        <input
+          onChange={e => this.firstChange(e.target.value)}
+          className="inputLine"
+          placeholder="first number"
+        />
+        {/* <p>{this.state.firstInput}</p> */}
+        <input
+          onChange={e => this.secondChange(e.target.value)}
+          className="inputLine"
+          placeholder="second number"
+        />
+        {/* <p>{this.state.secondInput}</p> */}
+        <button
+          onClick={() => {
+            this.addSum(this.state.firstInput, this.state.secondInput);
+          }}
+          className="confirmationButton"
+        >
+          Click for Sum
+        </button>
+        <span className="resultsBox">Sum:{this.state.results}</span>
+      </div>
+    );
   }
 }
 
